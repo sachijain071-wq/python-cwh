6c762c02e6adca05f0895a02e2c1772b59a29533
sachi jain <sachijain071@gmail.com>
2026-01-08 16:03:37 +0530

    new code

--- practice2.py
+++ practice2.py
@@ -1,3 +1,9 @@
-n=int(input("Enter a number:"))
-for i in range(1,11):
-    print(f" {n} x {i}={n*i}")
+# =int(input("Enter a number:"))
+# for i in range(11):
+#     print(f" {n} x {i}={n*i}")
+
+n=int(input("enter a number: "))
+for i in range(2,n): 
+    if(n%i==0):
+        print("it is not prime")
+        break
