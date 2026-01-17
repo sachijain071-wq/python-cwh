d6c6692969c53d2b17198d94b27ec002082f4bed
sachi jain <sachijain071@gmail.com>
2026-01-18 01:11:00 +0530

    another code using fn

--- practice7.py
+++ practice7.py
@@ -2,7 +2,10 @@
 #     print("good day ")
 # greet()  
 # print("kiki")  
-a=input("enter your name: ")
-def kiki():
-    print(f"goood day {a}")
-kiki()    
+# a=input("enter your name: ")
+# def kiki():
+#     print(f"goood day {a}")
+# kiki()   
+def goodDay(name):
+    print("good day" + name )
+goodDay("lolo")    
