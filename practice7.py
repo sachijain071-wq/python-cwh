ff72427a246d6b9d3695cfa96eb5f4583f13ef8f
sachi jain <sachijain071@gmail.com>
2026-01-17 21:45:55 +0530

    again a new program

--- practice7.py
+++ practice7.py
@@ -1,3 +1,8 @@
-def greet():
-    print("good day ")
-greet()    
+# def greet():
+#     print("good day ")
+# greet()  
+# print("kiki")  
+a=input("enter your name: ")
+def kiki():
+    print(f"goood day {a}")
+kiki()    
