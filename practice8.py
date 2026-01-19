72a59ca0029396b8a52ff710feedeb35ea674246
sachi jain <sachijain071@gmail.com>
2026-01-19 16:54:35 +0530

    learnt my frist recursion program on factorial

--- practice8.py
+++ practice8.py
@@ -1,4 +1,10 @@
-def hye(name,surname,ending="thankyou"):
-    print("good morning " + name + surname)
-    print(ending)
-hye("kiki " , " jain")
+# def hye(name,surname,ending="thankyou"):
+#     print("good morning " + name + surname)
+#     print(ending)
+# hye("kiki " , " jain")
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    return n* factorial(n-1)
+n=int(input("enter  a number: "))
+print(factorial(n))
