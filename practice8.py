b38e2bd2bb2e2025f999d2763e924fee7ae16d05
sachi jain <sachijain071@gmail.com>
2026-01-19 17:08:20 +0530

    sum of n natural number using recusion

--- practice8.py
+++ practice8.py
@@ -2,9 +2,9 @@
 #     print("good morning " + name + surname)
 #     print(ending)
 # hye("kiki " , " jain")
-def factorial(n):
-    if(n==0 or n==1):
-        return 1
-    return n* factorial(n-1)
+def sum(n):
+    if(n==0):
+        return 0
+    return  sum(n-1) + n
 n=int(input("enter  a number: "))
-print(factorial(n))
+print(sum(n))
