06412c03e3ec0b411def0943e1b51fcb2928596d
sachi jain <sachijain071@gmail.com>
2026-01-20 15:44:51 +0530

    created a class

--- practice9.py
+++ practice9.py
@@ -1,41 +1,47 @@
-import random 
+# import random 
 
-computer=random.choice([1,-1,0])
-youstr=input("enter your choice: ")
-youDict={"s": 1, "w": -1, "g": 0}
-revDict={1: "snake", -1: "water", 0: "gun"}
+# computer=random.choice([1,-1,0])
+# youstr=input("enter your choice: ")
+# youDict={"s": 1, "w": -1, "g": 0}
+# revDict={1: "snake", -1: "water", 0: "gun"}
 
-you=youDict[youstr]
+# you=youDict[youstr]
 
-print(f" you choose {revDict[you]}\n computer choose{revDict[computer]}")
+# print(f" you choose {revDict[you]}\n computer choose{revDict[computer]}")
 
-if(computer== you):
-    print("its a draw")
+# if(computer== you):
+#     print("its a draw")
 
-else:
-    if(computer== -1 and you == 1):
-        print("you win")
+# else:
+#     if(computer== -1 and you == 1):
+#         print("you win")
 
-    elif(computer == -1 and you ==0):
-        print("you loose")
+#     elif(computer == -1 and you ==0):
+#         print("you loose")
 
-    elif(computer == 1 and you == -1):
-        print("you loose")
+#     elif(computer == 1 and you == -1):
+#         print("you loose")
 
-    elif(computer == 1 and you ==0):
-        print("you win")
+#     elif(computer == 1 and you ==0):
+#         print("you win")
 
-    elif(computer == 0 and you == -1):
-        print("you win")
+#     elif(computer == 0 and you == -1):
+#         print("you win")
 
-    elif(computer == 0 and you == 1):
-        print("you loose")
+#     elif(computer == 0 and you == 1):
+#         print("you loose")
 
-    else:
-        print("somthing went wrong")
-
-
+#     else:
+#         print("somthing went wrong")
 
 
+
+class Employee:
+    name="kiki"
+    language="python"
+
+kiki=Employee()
+print(kiki.name)
+print(kiki.language)
 
     
