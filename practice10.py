9bb72c782137259d914cf17b7e0204e4b4dd6927
sachi jain <sachijain071@gmail.com>
2026-01-21 19:32:37 +0530

    how to cal the area and perimeter of a rect using self and init constructor

--- practice10.py
+++ practice10.py
@@ -1,23 +1,42 @@
-class person:
-    def __init__(self,name , age):
-        self.name=name
-        self.age=age
+# class person:
+#     def __init__(self,name , age):
+#         self.name=name
+#         self.age=age
     
-    def greet(self):
-        print("hello" , self.name)
+#     def greet(self):
+#         print("hello" , self.name)
 
-result=person("kiki", 22)
-result.greet()
+# result=person("kiki", 22)
+# result.greet()
 
-class BankAcc:
-    def __init__(self, acc_holder, balance):
-        self.acc_holder= acc_holder
-        self.balance= balance
+# class BankAcc:
+#     def __init__(self, acc_holder, balance):
+#         self.acc_holder= acc_holder
+#         self.balance= balance
 
-    def greete(self):
-        print(f" hye {self.acc_holder} your balance is {self.balance}")
+#     def greete(self):
+#         print(f" hye {self.acc_holder} your balance is {self.balance}")
 
 
-answer=BankAcc("kiki",300000)
-answer.greete()
+# answer=BankAcc("kiki",300000)
+# answer.greete()
+
+class Cal:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    # def value(self, length, breadth):
+    #     self.length= length
+    #     self.breadth= breadth
+
+    def area(self):
+        print(f"the area of rect is {self.x * self.y}")
+
+    def perimeter(self):
+        print(f" the perimeter of rect is {2* (self.x+ self.y)}")
+
+res=Cal(2,3)
+res.area()
+res.perimeter()
         
