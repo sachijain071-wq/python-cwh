8058145d76816a840357ef4999a30647c9c94e72
sachi jain <sachijain071@gmail.com>
2026-01-23 19:26:22 +0530

    practicing

---
+++ practice12.py
@@ -0,0 +1,23 @@
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+def give_bonus(employee):
+    if employee.salary < 50000:
+        employee.salary *= 1.10
+    else:
+        employee.salary *= 1.05
+
+emp = Employee("Emma", 40000)
+give_bonus(emp)
+print(emp.salary)
+
