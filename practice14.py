008d6e52f8381177a82c08cbfab30fbe7e158235
sachi jain <sachijain071@gmail.com>
2026-01-26 18:51:57 +0530

    used mulilevel in heritance

--- practice14.py
+++ practice14.py
@@ -1,10 +1,27 @@
-class Parent:
-    def display(self):
-        print("Parent display")
+# class Parent:
+#     def display(self):
+#         print("Parent display")
 
-class Child(Parent):
-    def display(self):
-        print("Child display")
+# class Child(Parent):
+#     def display(self):
+#         print("Child display")
 
-obj = Child()
-obj.display()
+# obj = Child()
+# obj.display()
+
+class A:
+    def show_a(self):
+        print("Class A")
+
+class B(A):
+    def show_b(self):
+        print("Class B")
+
+class C(B):
+    def show_c(self):
+        print("Class C")
+
+obj = C()
+obj.show_a()
+obj.show_b()
+obj.show_c()
