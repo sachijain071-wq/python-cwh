737cf0eb4ee69cacb8d5ea8a8b0008903a149fff
sachi jain <sachijain071@gmail.com>
2026-01-29 14:44:10 +0530

    using super

---
+++ practice15.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+s = Student("Alice", 20, 101)
+print(s.name, s.age, s.student_id)
