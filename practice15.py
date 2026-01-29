f07b3cc26032a426123c0a38847945b424c0e4f8
sachi jain <sachijain071@gmail.com>
2026-01-29 14:45:14 +0530

    method overidding

--- practice15.py
+++ practice15.py
@@ -1,12 +1,11 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# 
+class Vehicle:
+    def fuel_type(self):
+        print("Uses fuel")
 
-class Student(Person):
-    def __init__(self, name, age, student_id):
-        super().__init__(name, age)
-        self.student_id = student_id
+class Car(Vehicle):
+    def fuel_type(self):
+        print("Uses petrol or diesel")
 
-s = Student("Alice", 20, 101)
-print(s.name, s.age, s.student_id)
+c = Car()
+c.fuel_type()
