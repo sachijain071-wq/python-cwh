af83d913712ff3fd6e65a04dd9599ad90c136b38
sachi jain <sachijain071@gmail.com>
2026-01-30 16:04:39 +0530

    abstraction basics

--- practice16.py
+++ practice16.py
@@ -1,13 +1,18 @@
-class Bird:
-    def fly(self):
-        print("Bird flies using wings")
+# 
 
-class Airplane:
-    def fly(self):
-        print("Airplane flies using engines")
+from abc import ABC, abstractmethod
 
-def make_fly(obj):
-    obj.fly()
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
 
-make_fly(Bird())
-make_fly(Airplane())
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+s = Square(4)
+print(s.area())
